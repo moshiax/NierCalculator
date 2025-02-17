f2f4e28d7497adc68c4e2489758a29aa2a12a498
sheva <olegbeznogy@gmail.com>
2025-02-17 04:35:40 +0300

    <3

---
+++ songs.js
@@ -0,0 +1,263 @@
+const phrases = {
+	// "Weight of the World"
+	'end of yorha': { 
+		audio: { src: 'music/Weight of the World the End of YoRHa', artist: 'Keiichi Okabe, Vocals & Chorus: YoRHa', title: 'Weight of the World the End of YoRHa' }, 
+		background: { 'backgrounds/background01.jpg': {} }, 
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+
+	'weight world': { 
+		audio: { src: 'music/Weight of the World⧸English Version', artist: 'Keiichi Okabe, Vocals: J\'Nique Nicole', title: 'Weight of the World English Version' }, 
+		background: { 'backgrounds/background02.jpg': {} }, 
+		backgroundAndroid: { 'backgrounds/android01.jpg': {} }
+	},
+
+	'jap': { 
+		audio: { src: 'music/Weight of the World Japanese', artist: 'Keiichi Okabe, Vocals: Marina Kawano', title: 'Weight of the World Kowaretasekainouta' }, 
+		background: { 'backgrounds/background02.jpg': {} }, 
+		backgroundAndroid: { 'backgrounds/android01.jpg': {} }
+	},
+
+	'fr': { 
+		audio: { src: 'music/Weight of the World French', artist: 'Keiichi Okabe, Vocals: Emi Evans', title: 'Weight of the World Nouveau - FR Version' }, 
+		background: { 'backgrounds/background02.jpg': {} }, 
+		backgroundAndroid: { 'backgrounds/android01.jpg': {} }
+	},
+	// "Voice of no Return"
+	'voice return': { 
+		audio: { src: 'music/Voice of no Return - Guitar', artist: 'Keiichi Okabe, Vocals?: Emi Evans', title: 'Voice of No Return - Guitar' }, 
+		background: { 'backgrounds/background11.jpg': {} }, 
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+
+	'normal': { 
+		audio: { src: 'music/Voice of no Return - Normal', artist: 'Keiichi Okabe, Vocals?: Emi Evans', title: 'Voice of No Return - Normal' }, 
+		background: { 'backgrounds/background11.jpg': {} }, 
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	
+	// "Song of the Ancients"
+	'atonement': { 
+		audio: { src: 'music/Song of the Ancients - Atonement', artist: 'Keiichi Okabe, Vocals?: Emi Evans', title: 'Song of the Ancients - Atonement' }, 
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} }, 
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+
+	'song ancients': { 
+		audio: { src: 'music/Song of the Ancients - Atonement', artist: 'Keiichi Okabe, Vocals?: Emi Evans', title: 'Song of the Ancients - Atonement' }, 
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} }, 
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+
+	// "Wretched Weaponry"
+	'dynamic': {
+		audio: { src: 'music/Wretched Weaponry Dynamic', title: 'Wretched Weaponry: Medium/Dynamic', artist: 'Keiichi Okabe, Vocals?: Emi Evans, Nami Nakagawa' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'quiet': {
+		audio: { src: 'music/Wretched Weaponry Quiet', title: 'Wretched Weaponry: Quiet', artist: 'Keiichi Okabe, Vocals?: Emi Evans' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'wretched weaponry': {
+		audio: { src: 'music/Wretched Weaponry Dynamic', title: 'Wretched Weaponry: Medium/Dynamic', artist: 'Keiichi Okabe, Vocals?: Emi Evans, Nami Nakagawa' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+
+	// Fights
+	'bipolar nightmare': { 
+		audio: { src: 'music/Bipolar Nightmare', artist: 'Keigo Hoashi, Vocals?: Nami Nakagawa, NieR:Automata Choir', title: 'Bipolar Nightmare' }, 
+		background: { 'backgrounds/background04.jpg': { author: 'Square Enix', link: 'https://www.square-enix.com' } }, 
+		backgroundAndroid: { 'backgrounds/android04.jpg': { author: '水水人', link: 'https://www.pixiv.net/en/users/2201552' } }
+	},
+	'significance': {
+		audio: { src: 'music/Significance', title: 'Significance', artist: 'Keiichi Okabe, Vocals?: Marina Kawano, Emi Evans' },
+		background: { 'backgrounds/background03.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'beautiful song': {
+		audio: { src: 'music/Beautiful Song', title: 'A Beautiful Song', artist: 'Keigo Hoashi, Vocals?: Emi Evans, J\'Nique Nicole, NieR:Automata Choir' },
+		background: { 'backgrounds/background10.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'dark colossus': {
+		audio: { src: 'music/Dark Colossus - Kaiju', title: 'Dark Colossus - Kaiju', artist: 'Keigo Hoashi & Keiichi Okabe, Vocals?: Emi Evans' },
+		background: { 'backgrounds/darkcolossus.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'war war': {
+		audio: { src: 'music/War & War', title: 'War & War', artist: 'Keigo Hoashi & Keiichi Okabe, Vocals?: NieR:Automata Choir' },
+		background: { 'backgrounds/background.jpg': {} },
+		backgroundAndroid: { 'backgrounds/android03.jpg': {} }
+	},
+	'grandma destruction': {
+		audio: { src: 'music/Grandma - Destruction', title: 'Grandma - Destruction', artist: 'Kuniyuki Takahashi & Keiichi Okabe, Vocals?: NieR:Automata Choir' },
+		background: { 'backgrounds/background13.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+
+	// Places
+	'rays light': {
+		audio: { src: 'music/City Ruins - Rays of Light', title: 'City Ruins - Rays of Light', artist: 'Keiichi Okabe, Vocals?: J\'Nique Nicole' },
+		background: { 'backgrounds/background05.jpg': {} },
+		backgroundAndroid: { 'backgrounds/android05.jpg': {} }
+	},
+	'shade': {
+		audio: { src: 'music/City Ruins - Shade', title: 'City Ruins - Shade', artist: 'Keiichi Okabe, Vocals?: Emi Evans' },
+		background: { 'backgrounds/shade.jpg': { author: 'ciel', link: 'https://www.pixiv.net/en/users/24924426' } },
+		backgroundAndroid: { 'backgrounds/android08.jpg': { author: 'Kazuma Koda', link: 'https://x.com/Lownine' } }
+	},
+	'copied city': {
+		audio: { src: 'music/Copied City', title: 'Copied City', artist: 'Keigo Hoashi, Vocals?: Nami Nakagawa' },
+		background: { 'backgrounds/copiedcity.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'memories dust': {
+		audio: { src: 'music/Memories of Dust', title: 'Memories of Dust', artist: 'Kuniyuki Takahashi, Vocals?: J\'Nique Nicole' },
+		background: { 'backgrounds/background07.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'forest kingdom': {
+		audio: { src: 'music/Forest Kingdom', title: 'Forest Kingdom', artist: 'Keigo Hoashi, Vocals?: Nami Nakagawa' },
+		background: { 'backgrounds/background02.jpg': {} },
+		backgroundAndroid: { 'backgrounds/android02.jpg': { author: 'SIRDINHOSAUR', link: 'https://sirdinhosaur.carrd.co' } }
+	},
+	'pascal': {
+		audio: { src: 'music/Pascal', title: 'Pascal', artist: 'Keiichi Okabe, Vocals?: Saki Ishii' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'amusement park': {
+		audio: { src: 'music/Amusement Park', title: 'Amusement Park', artist: 'Keigo Hoashi, Vocals?: Emi Evans' },
+		background: { 'backgrounds/background09.jpg': {} },
+		backgroundAndroid: { 'backgrounds/android09.jpg': {} }
+	},
+	'tower': {
+		audio: { src: 'music/The Tower', title: 'The Tower', artist: 'Keiichi Okabe, Vocals?: Emi Evans, J\'Nique Nicole' },
+		background: { 'backgrounds/tower.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'sound end': {
+		audio: { src: 'music/The Sound of the End', title: 'The Sound of the End', artist: 'Keigo Hoashi, Vocals?: Nami Nakagawa' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'alien': {
+		audio: { src: 'music/Alien Manifestation', title: 'Alien Manifestation', artist: 'Keigo Hoashi & Keiichi Okabe, Vocals?: Nami Nakagawa' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+
+	// Ambient
+	'peaceful sleep': {
+		audio: { src: 'music/Peaceful Sleep', title: 'Peaceful Sleep', artist: 'Keiichi Okabe, Vocals?: Emi Evans' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'birth wish': {
+		audio: { src: 'music/Birth of a Wish', title: 'Birth of a Wish', artist: 'Keiichi Okabe, Vocals?: NieR:Automata Choir' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'dawn breeze': {
+		audio: { src: 'music/Faltering Prayer - Dawn Breeze', title: 'Faltering Prayer - Dawn Breeze', artist: 'Keigo Hoashi & Keiichi Okabe, Vocals?: Emi Evans' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'cold rain': {
+		audio: { src: 'music/Vague Hope - Cold Rain', title: 'Vague Hope - Cold Rain', artist: 'Keigo Hoashi, Vocals?: Emi Evans' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'spring rain': {
+		audio: { src: 'music/Vague Hope - Spring Rain', title: 'Vague Hope - Spring Rain', artist: 'Keigo Hoashi, Vocals?: Emi Evans' },
+		background: { 'backgrounds/springrain.jpg': { author: 'Square Enix', link: 'https://www.square-enix.com' } },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'nothing': {
+		audio: { src: 'music/Significance - Nothing', title: 'Significance - Nothing', artist: 'Keiichi Okabe, Vocals?: J\'Nique Nicole' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'rebirth hope': {
+		audio: { src: 'music/Rebirth & Hope', title: 'Rebirth & Hope', artist: 'Keigo Hoashi, Vocals?: Emi Evans, Nami Nakagawa, Marina Kawano' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'treasured times': {
+		audio: { src: 'music/Treasured Times', title: 'Treasured Times', artist: 'Keigo Hoashi, Vocals?: Saki Ishii' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'end unknown': {
+		audio: { src: 'music/End of the Unknown', title: 'End of the Unknown', artist: 'Keiichi Okabe, Vocals?: NieR:Automata Choir' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'starry sky': {
+		audio: { src: 'music/Starry Sky', title: 'Starry Sky', artist: 'Keigo Hoashi & Keiichi Okabe, Vocals?: Emi Evans' },
+		background: { 'backgrounds/starrysky.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+
+	// Emil
+	'emil shop': {
+		audio: { src: 'music/Emils Shop', title: 'Emil\'s Shop', artist: 'Kuniyuki Takahashi & Kakeru Ishihama, Vocals?: Mai Kadowaki' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'emil despair': {
+		audio: { src: 'music/Emil - Despair', title: 'Emil - Despair', artist: 'Keigo Hoashi & Kakeru Ishihama, Vocals?: Nami Nakagawa' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+
+	// Other
+	'crumbling lies': {
+		audio: { src: 'music/Crumbling Lies', title: 'Crumbling Lies', artist: 'Keigo Hoashi, Vocals?: NieR:Automata Choir' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'fortress lies': {
+		audio: { src: 'music/Fortress of Lies', title: 'Fortress of Lies', artist: 'Keiichi Okabe, Vocals?: Emi Evans' },
+		background: { 'backgrounds/fortressoflies.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'widespread illness': {
+		audio: { src: 'music/Widespread Illness', title: 'Widespread Illness', artist: 'Keigo Hoashi' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'blissful death': {
+		audio: { src: 'music/Blissful Death', title: 'Blissful Death', artist: 'Keiichi Okabe' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'mourning': {
+		audio: { src: 'music/Mourning', title: 'Mourning', artist: 'Keiichi Okabe, Vocals?: NieR:Automata Choir' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'dependent weakling': {
+		audio: { src: 'music/Dependent Weakling', title: 'Dependent Weakling', artist: 'Kuniyuki Takahashi, Vocals?: NieR:Automata Choir' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'possessed disease': {
+		audio: { src: 'music/Possessed by Disease', title: 'Possessed by Disease', artist: 'Keiichi Okabe, Vocals?: Nami Nakagawa' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'broken heart': {
+		audio: { src: 'music/Broken Heart', title: 'Broken Heart', artist: 'Keigo Hoashi, Vocals?: Emi Evans' },
+		background: { 'backgrounds/!!!!!!!!!.jpg': {} },
+		backgroundAndroid: { 'backgrounds/!!!!!!!!!.jpg': {} }
+	},
+	'happy new year': {
+		audio: { src: 'music/Weight of the World Japanese', title: 'Weight of the World (Japanese)', artist: 'Keiichi Okabe, Vocals: Marina Kawano' },
+		background: { 'bonus/winterpc.jpg': { author: 'Square Enix', link: 'https://www.square-enix.com' } },
+		backgroundAndroid: { 'bonus/winter.jpg': { author: 'tricoliet', link: 'https://x.com/tricoliet' } }
+	}
+};
